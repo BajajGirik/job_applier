fb4d87ea14c38b16641794f927a5cbd700818115
BajajGirik <bajajgirik2010@gmail.com>
2025-03-02 13:19:25 +0530

    feat: load web browser with default profile

--- job_applier/web_browser.py
+++ job_applier/web_browser.py
@@ -1,14 +1,26 @@
 from playwright.async_api import async_playwright
 from utils.env_helper import get_env
+from utils.copy import copy_directory
 
 class WebBrowser:
     def __init__(self) -> None:
-        self.__user_data_dir = get_env("USER_DATA_DIR")
+        # I accidentally corrupted my original profile while experimenting :')
+        # So creating a copy of `user_data_dir` to keep the origin profile untouched
+        self.__user_data_dir = copy_directory(get_env("USER_DATA_DIR"))
 
 
     async def setup(self) -> None:
         self.__playwright = await async_playwright().start()
-        self.__context = await self.__playwright.chromium.launch_persistent_context(self.__user_data_dir, channel="chrome", headless=False)
+        self.__context = await self.__playwright.chromium.launch_persistent_context(
+            self.__user_data_dir,
+            channel="chrome",
+            user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+            headless=False,
+            no_viewport=True,
+            args= [
+                "--start-maximized",
+            ]
+        )
 
 
     async def destroy(self) -> None:
