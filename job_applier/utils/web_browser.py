56c9ac57c46ecd7c970a04dec4ac47fc00f74c38
BajajGirik <bajajgirik2010@gmail.com>
2025-03-02 13:50:33 +0530

    refactor!: restructure web browser class

--- job_applier/utils/web_browser.py
+++ job_applier/utils/web_browser.py
@@ -1,20 +1,27 @@
-from playwright.async_api import ElementHandle, async_playwright
+from playwright.async_api import ElementHandle, async_playwright, Playwright, BrowserContext, Page
 from utils.env_helper import get_env
 from utils.copy import copy_directory
 from playwright_stealth import stealth_async
 
 
 class WebBrowser:
-    def __init__(self) -> None:
+    def __init__(self, user_data_dir: str, playwright: Playwright, context: BrowserContext, page: Page) -> None:
+        self.__user_data_dir = user_data_dir
+        self.__playwright = playwright
+        self.__context = context
+        self.__page = page
+
+
+    @classmethod
+    async def create(cls):
         # I accidentally corrupted my original profile while experimenting :')
         # So creating a copy of `user_data_dir` to keep the origin profile untouched
-        self.__user_data_dir = copy_directory(get_env("USER_DATA_DIR"))
+        user_data_dir = copy_directory(get_env("USER_DATA_DIR"))
 
+        playwright = await async_playwright().start()
 
-    async def setup(self) -> None:
-        self.__playwright = await async_playwright().start()
-        self.__context = await self.__playwright.chromium.launch_persistent_context(
-            self.__user_data_dir,
+        context = await playwright.chromium.launch_persistent_context(
+            user_data_dir,
             channel="chrome",
             user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
             headless=False,
@@ -24,9 +31,11 @@ class WebBrowser:
             ]
         )
 
-        self.__page = await self.__context.new_page()
-        await stealth_async(self.__page)
+        page = await context.new_page()
+        await stealth_async(page)
 
+
+        return cls(user_data_dir, playwright, context, page)
 
 
     async def destroy(self) -> None:
